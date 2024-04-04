fff9d9e534f51e3285d77c5f58f37af6ad85d4a4
Leon <leon.l.lysak@gmail.com>
2024-04-04 12:57:04 -0400

    formatted cdp.nim exports to match imports

--- src/cdp.nim
+++ src/cdp.nim
@@ -4,8 +4,10 @@
 
 import core/[chrome, base, browser],
         domains/[target, security, runtime, profiler, performance,
+                page, network, log, io, input, fetch, emulation, domdebugger,
+                dom, debugger, browser_domain
+                ]
+export chrome, base, browser,
+        target, security, runtime, profiler, performance,
         page, network, log, io, input, fetch, emulation, domdebugger,
-        dom, debugger, browser_domain]
-export chrome, base, browser, target, security, runtime, profiler,
-    performance, page, network, log, io, input, fetch, emulation,
-    domdebugger, dom, debugger,browser_domain
+        dom, debugger,browser_domain
