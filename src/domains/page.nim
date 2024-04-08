1d3fd90fac35e48d400509c781a90c214a4d8b63
Leon Lysak <leon.l.lysak@gmail.com>
2024-04-07 23:11:27 -0400

    Updated page domain API to include proper method

--- src/domains/page.nim
+++ src/domains/page.nim
@@ -21,15 +21,15 @@ type
         loadEventFired = "Page.loadEventFired"
         windowOpen = "Page.windowOpen"
 
-proc addScriptToEvaluateOnLoad*(tab: Tab; source: string; params: JsonNode): Future[JsonNode] {.async.} =
-    ## `Page.addScriptToEvaluateOnLoad
-    ## <https://chromedevtools.github.io/devtools-protocol/1-3/Page/#method-addScriptToEvaluateOnLoad>`_
+proc addScriptToEvaluateOnNewDocument*(tab: Tab; source: string; params: JsonNode): Future[JsonNode] {.async.} =
+    ## `Page.addScriptToEvaluateOnNewDocument
+    ## <https://chromedevtools.github.io/devtools-protocol/1-3/Page/#method-addScriptToEvaluateOnNewDocument>`_
     ##
     ## Evaluates given script in every frame upon creation (before loading frame's scripts).
     params["source"] = newJString(source)
-    result = await tab.sendCommand("Page.addScriptToEvaluateOnLoad", params)
-proc addScriptToEvaluateOnLoad*(tab: Tab; source: string): Future[JsonNode] {.async.} =
-    result = await tab.sendCommand("Page.addScriptToEvaluateOnLoad", %*{"source": source})
+    result = await tab.sendCommand("Page.addScriptToEvaluateOnNewDocument", params)
+proc addScriptToEvaluateOnNewDocument*(tab: Tab; source: string): Future[JsonNode] {.async.} =
+    result = await tab.sendCommand("Page.addScriptToEvaluateOnNewDocument", %*{"source": source})
 
 proc bringToFront*(tab: Tab) {.async.} =
     ## `Page.bringToFront <https://chromedevtools.github.io/devtools-protocol/1-3/Page/#method-bringToFront>`_
@@ -183,9 +183,3 @@ proc stopLoading*(tab: Tab) {.async.} =
     ##
     ## Force the page stop all navigations and pending resource fetches.
     discard await tab.sendCommand("Page.stopLoading")
-
-proc addScriptToEvaluateOnNewDocument*(tab: Tab; script: string): Future[JsonNode] {.async.} =
-    ## `Page.addScriptToEvaluateOnNewDocument <https://chromedevtools.github.io/devtools-protocol/1-3/Page/#method-addScriptToEvaluateOnNewDocument>`_
-    ##
-    ## Evaluates given script in every frame upon creation (before loading frame's scripts).
-    result = await tab.sendCommand("Page.addScriptToEvaluateOnNewDocument", %*{"source": script})
